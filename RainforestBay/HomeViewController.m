8c6d6292d455b459c83baebe0126694307a5a5b7
Husein Kareem <huseinkareem@gmail.com>
2015-07-26 16:36:42 -0400

    homeVC initial layout set

--- RainforestBay/HomeViewController.m
+++ RainforestBay/HomeViewController.m
@@ -9,29 +9,17 @@
 #import "HomeViewController.h"
 
 @interface HomeViewController ()
-
+@property (weak, nonatomic) IBOutlet UIButton *signUpButton;
+@property (weak, nonatomic) IBOutlet UIButton *howItWorksButton;
 @end
 
 @implementation HomeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    //view setup
+    self.signUpButton.layer.cornerRadius = 5.0;
+    self.howItWorksButton.layer.cornerRadius = 5.0;
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
